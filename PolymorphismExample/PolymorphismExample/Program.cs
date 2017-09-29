0ddb83f5684c20aa75b64f0e1649b7708ed6419c
KiranPaul26 <31848182+KiranPaul26@users.noreply.github.com>
2017-09-29 15:53:31 +0100

    Add files via upload

---
+++ PolymorphismExample/PolymorphismExample/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PolymorphismExample
+{
+    class Polygon
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Drawing a Polygon.");
+        }
+    }
+    class Rectangle : Polygon
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing a Rectangle.");
+        }
+    }
+    class Triangle : Polygon
+    {
+        public new void Draw()
+        {
+            Console.WriteLine("Drawing a Triangle.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Polygon> polygons = new List<Polygon>();
+            polygons.Add(new Polygon());
+            polygons.Add(new Rectangle());          //rectangle that has been cast as a polygon
+            polygons.Add(new Triangle());           //triangle that has been cast as a polygon
+
+            foreach (Polygon p in polygons)
+            {
+                p.Draw();
+            }
+
+            Triangle triangle1 = new Triangle();
+            triangle1.Draw();
+        }
+    }
+}
